c6e26b7158e4cfb516838a23eaf44a9d78338282
serjsulim <serjsulim@gmail.com>
2026-04-11 19:58:26 +0300

    FPS

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def run():
     
     running = True     #    змінна, яка показує, коли зупинити гру
     pygame.init()      # ініціюємо модуль pygame
-    
+    clock = pygame.time.Clock()  # змінна для створення FPS
     screen = pygame.display.set_mode((WIDTH, HEIGHT)) # створюємо вікно 
     pygame.display.set_caption('Arkanoid')           # заголовок вікна
     raketka = Raketka(screen)                  # створюємо ракетку з рядка 3 імпорту з файла ракетка
@@ -19,7 +19,9 @@ def run():
         controls.events(screen, raketka)           # відслідковуємо натискання клавіш для руху ракетки
         raketka.update_raketka()                   # оновити положення ракетки ()              
         controls.update(BG_COLOR, screen, raketka)
-       
+        
+        clock.tick(FPS)  # вказуємо, щоб даний цикл while виконувався FPS раз на секунду
+
 
 run()
 
